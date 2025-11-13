ddacc2badfb3be976f24b16898ada63ce7a312f1
Szymon Madeja <108612900+SzymonMadeja@users.noreply.github.com>
2025-11-13 19:28:00 +0100

    Delete zadanie_1.py

--- zadanie_1.py
+++
@@ -1,8 +0,0 @@
-wynik = int(input("Podaj wynik egzaminu (ile procent): "))
-
-if wynik >= 80:
-    print("Zdano egzamin w terminie 0")
-elif wynik >= 50 and wynik <=80:
-    print("Możesz poprawić wynik egzaminu")
-else:
-    print("Musisz poprawić wynik egzaminu")
